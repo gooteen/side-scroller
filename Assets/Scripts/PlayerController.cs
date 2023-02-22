7bbd66b88723bdfd9a5a9247627cad01131bf7dc
gooteen <108371872+gooteen@users.noreply.github.com>
2023-02-22 23:11:33 +0300

    Configure weapon rotation control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,36 +7,95 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform _arm;
     [SerializeField] private Camera _cam;
 
+    [SerializeField] private SpriteController _spriteController;
     [SerializeField] private PlayerSettings _settings;
 
     [SerializeField] private float _weaponRotationLimitAngle;
 
+    [SerializeField] private bool _isFacingRight;
+
+    private void Start()
+    {
+        if (_arm.localEulerAngles.z < 90 || (_arm.localEulerAngles.z > 270 && _arm.localEulerAngles.z < 360))
+        {
+            _isFacingRight = true;
+        }
+        else
+        {
+            _isFacingRight = false;
+            _spriteController.FlipWeaponSprite();
+        }
+    }
+
     void Update()
     {
-        if(InputProcessor.Instance.GetMouseDelta().magnitude != 0)
-        {
-            _arm.Rotate(0, 0, InputProcessor.Instance.GetMouseDelta().normalized.y * _settings._mouseSensitivity);
-            Debug.Log("euler " + _arm.eulerAngles.z);
-            Debug.Log("rotation " + _arm.rotation.z);
+        Aim();
+    }
 
-           
-            if (_arm.eulerAngles.z < 360 - _weaponRotationLimitAngle && _arm.rotation.z < 0)
+    private void Aim()
+    {
+        if (InputProcessor.Instance.GetMouseDelta().magnitude != 0)
+        {
+            _arm.localRotation = Quaternion.AngleAxis(GetRotationAngle(), Vector3.forward);
+            SetDirectionOfAim();
+            Debug.Log("EULER: " + _arm.localEulerAngles.z);
+            Debug.Log("Mouse Pos: " + InputProcessor.Instance.GetMousePosition());
+        }
+    }
+
+    private void SetDirectionOfAim()
+    {
+        if (InputProcessor.Instance.GetMousePosition().x < Screen.width / 2)
+        {
+            if (_isFacingRight)
             {
-            _arm.rotation = Quaternion.Euler(_arm.eulerAngles.x, _arm.eulerAngles.y, -_weaponRotationLimitAngle);
+                ChangeSide();
             }
 
-            if (_arm.eulerAngles.z > _weaponRotationLimitAngle && _arm.rotation.z > 0)
+            if (_arm.eulerAngles.z < 90 + _weaponRotationLimitAngle && _arm.localRotation.z > 0)
             {
-            _arm.rotation = Quaternion.Euler(_arm.eulerAngles.x, _arm.eulerAngles.y, _weaponRotationLimitAngle);
+                _arm.localRotation = Quaternion.Euler(_arm.localEulerAngles.x, _arm.localEulerAngles.y, 90 + _weaponRotationLimitAngle);
+            }
+
+            if (_arm.eulerAngles.z > 270 - _weaponRotationLimitAngle && _arm.localRotation.z < 0)
+            {
+                Debug.Log("Hey2: " + _arm.eulerAngles.z + " > " + _weaponRotationLimitAngle + "&&" + _arm.rotation.z + " > 0");
+                _arm.localRotation = Quaternion.Euler(_arm.localEulerAngles.x, _arm.localEulerAngles.y, 270 - _weaponRotationLimitAngle);
             }
         }
 
-        if (InputProcessor.Instance.GetMovementDirection().normalized.x > 0)
+        else if (InputProcessor.Instance.GetMousePosition().x > Screen.width / 2)
         {
-            _arm.rotation = new Quaternion(0, 0, _arm.rotation.z, 1);
-        } else if (InputProcessor.Instance.GetMovementDirection().normalized.x < 0)
-        {
-            _arm.rotation = new Quaternion(0, 179, _arm.rotation.z, 1);
+            if (!_isFacingRight)
+            {
+                ChangeSide();
+            }
+
+            if (_arm.eulerAngles.z < 360 - _weaponRotationLimitAngle && _arm.localRotation.z < 0)
+            {
+                _arm.localRotation = Quaternion.Euler(_arm.localEulerAngles.x, _arm.localEulerAngles.y, -_weaponRotationLimitAngle);
+            }
+
+            if (_arm.eulerAngles.z > _weaponRotationLimitAngle && _arm.localRotation.z > 0)
+            {
+                Debug.Log("Hey2: " + _arm.eulerAngles.z + " > " + _weaponRotationLimitAngle + "&&" + _arm.rotation.z + " > 0");
+                _arm.localRotation = Quaternion.Euler(_arm.localEulerAngles.x, _arm.localEulerAngles.y, _weaponRotationLimitAngle);
+            }
         }
+    }
+
+    private void ChangeSide()
+    {
+        _spriteController.FlipWeaponSprite();
+        _spriteController.FlipPlayerSprite();
+        _isFacingRight = !_isFacingRight;
+    }
+
+    private float GetRotationAngle()
+    {
+        Vector2 _dir = InputProcessor.Instance.GetMousePosition() - new Vector2(_cam.WorldToScreenPoint(_arm.position).x, _cam.WorldToScreenPoint(_arm.position).y);
+        float _angle = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
+        Debug.Log("angle: " + _angle);
+        return _angle;
     }
 }
---
+++ Assets/Scripts/SpriteController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _weaponRenderer;
+    private SpriteRenderer _playerRenderer;
+
+    void Awake()
+    {
+        _playerRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void FlipWeaponSprite()
+    {
+        _weaponRenderer.flipY = !_weaponRenderer.flipY;
+    }
+
+    public void FlipPlayerSprite()
+    {
+        _playerRenderer.flipX = !_playerRenderer.flipX;
+    }
+
+
+}
